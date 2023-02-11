110ea29140c2fbcfc038e3c1486ed2ca4290b848
Cindewi <112582932+dewismecindy@users.noreply.github.com>
2023-02-11 17:44:00 +0100

    Nettoyage Projet vierge React + création components/pages et imports

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+/* Les imports pages */
+import Home from "./pages/Home";
+import Comics from "./pages/Comics";
+import Favorites from "./pages/Favorites";
+import Characters from "./pages/Characters";
+
+/* Les imports  components*/
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
