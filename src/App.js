1e5e3433a85c364b467a261d78d76c3a994d7326
Cindewi <112582932+dewismecindy@users.noreply.github.com>
2023-02-13 16:15:23 +0100

    data

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 
 /* Les import de packages */
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import axios from "axios";
+import { useEffect } from "react";
 /* Les imports pages */
 import Home from "./pages/Home";
 import Comics from "./pages/Comics";
@@ -10,12 +11,24 @@ import Favorites from "./pages/Favorites";
 import Characters from "./pages/Characters";
 import Character from "./pages/Character";
 
-/* Les imports  components*/
+/* Les imports components*/
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 function App() {
+  function SaveData(data) {
+    let favoriteFromUser = [];
+    favoriteFromUser = JSON.parse(localStorage.getItem("favorite")) || [];
+
+    if (favoriteFromUser.some((id) => (id === data) === true)) {
+      return alert`Ce personnage est correctement ajouté à vos favoris`;
+    } else {
+      favoriteFromUser.push(data);
+      localStorage.setItem("favorite", JSON.stringify(favoriteFromUser));
+      alert`Ce personnage est dèjà ajouté à vos favoris`;
+    }
+  }
   return (
     <Router>
       <Routes>
@@ -23,7 +36,10 @@ function App() {
         <Route path="/comics" element={<Comics />}></Route>
         <Route path="/characters" element={<Characters />}></Route>
         <Route path="/favorites" element={<Favorites />}></Route>
-        <Route path="/character" element={<Character />}></Route>
+        <Route
+          path="/character"
+          element={<Character SaveData={SaveData} />}
+        ></Route>
       </Routes>
     </Router>
   );
