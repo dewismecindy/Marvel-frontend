89d425519a71a86c06ec03333fbb383d009c8e93
Cindewi <112582932+dewismecindy@users.noreply.github.com>
2023-02-14 16:44:21 +0100

    recherches Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,22 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [data, setData] = useState({});
+  const [isLoading, setisLoading] = useState({});
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://lereacteur-marvel-api.herokuapp.com/comics/${characterID}"
+        );
+        console.log(response.data.comics);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+  return;
 };
 export default Home;
