a2bcb18f442e41dfd36ccda9888117ed4bf092bb
Cindewi <112582932+dewismecindy@users.noreply.github.com>
2023-02-14 15:47:15 +0100

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="nav-bar">
+      <div className="button-nav-bar">
+        <Link to="/Characters">Personnages</Link>
+      </div>
+      <div className="button-nav-bar">
+        <Link to="/Comics">Comics</Link>
+      </div>
+      <div className="button-nav-bar">
+        <Link to="/Favorites">Favoris</Link>
+      </div>
+    </div>
+  );
 };
+
 export default Navbar;
