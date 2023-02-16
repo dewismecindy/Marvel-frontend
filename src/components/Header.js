165f0e5bb8c3d89fc5554d49b25886151a05662c
Cindewi <112582932+dewismecindy@users.noreply.github.com>
2023-02-16 15:18:00 +0100

    composer le header + search

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,39 @@
 import logo from "../assets/logositemarvel.png";
+import { Link } from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
+  const handleSearch = (event) => {
+    const value = event.target.value;
+    props.setSearch(value);
+  };
   return (
     <header>
-      <img src={logo} alt="logo Marvel" />
+      <Link to={"/"}>
+        <img src={logo} alt="logo Marvel" />
+      </Link>
+      <Link
+        to={"/"}
+        onClick={() => {
+          props.setSearch("");
+        }}
+      >
+        Characters
+      </Link>
+      <Link
+        to={"/comics"}
+        onClick={() => {
+          props.setSearch("");
+        }}
+      >
+        Comics
+      </Link>
+      <Link to={"/favoris"}>Favoris</Link>
+      <input
+        type="text"
+        placeholder="Search"
+        value={props.search}
+        onChange={handleSearch}
+      />
     </header>
   );
 };
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,4 +1,32 @@
-const Pagination = () => {
-  return <div>Pagination</div>;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Pagination = (page) => {
+  return (
+    <>
+      {page.counter !== 1 ? (
+        <p
+          onClick={() => {
+            if (page.counter !== 1) {
+              page.setCounter(page.counter - 1);
+            }
+          }}
+        >
+          <FontAwesomeIcon icon="arrow-left" className="arrow" />
+        </p>
+      ) : (
+        <div className="empty-div"></div>
+      )}
+
+      <p>{page.counter}</p>
+      <p
+        onClick={() => {
+          page.setCounter(page.counter + 1);
+        }}
+      >
+        <FontAwesomeIcon icon="arrow-right" className="arrow" />
+      </p>
+    </>
+  );
 };
+
 export default Pagination;
